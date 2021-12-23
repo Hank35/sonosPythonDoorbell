87dc888f4a823104fa68344e7637e7085e9c1015
Hein Crebolder <heincrebolder@gmail.com>
2021-12-23 10:56:09 +0100

    runs, still in baby shoes tho

---
+++ app.py
@@ -0,0 +1,26 @@
+import soco
+import time
+from flask import Flask
+
+app = Flask(__name__)
+doorbell = "https://raw.githubusercontent.com/jfarcher/esp8266-mqtt-sonos-doorbell/master/doorbell-1.mp3"
+
+
+@app.route('/ring')
+def ring():
+    device = soco.discovery.by_name("Penthouse")
+    track = device.get_current_track_info()
+    initial_playlist_position = int(track["playlist_position"]) - 1
+
+    initial_track_position = (track["position"])
+    initial_state = device.get_current_transport_info().get("current_transport_state")
+    initial_volume = device.volume
+    device.volume=15
+    device.play_uri(uri=doorbell, meta='')
+    time.sleep( 2 )
+    device.volume=initial_volume  
+    device.play_from_queue(initial_playlist_position)
+    device.seek(initial_track_position)
+    # doesnt seem to work if you stream from spotify yet.
+    if initial_state != 'PLAYING':
+                device.pause()
--- main.py
+++
@@ -1,6 +0,0 @@
-import soco
-
-devices = {device.player_name: device for device in soco.discover()}
-
-for device in devices:
-    print(device.by_name())
